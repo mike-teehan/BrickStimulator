f87518387b0b563c71b779488a1ecfd1a7241261
Mike Teehan <mike.teehan@gmail.com>
2016-03-14 04:47:02 -0400

    Moved the title background to _fixed_process() and added a "nudge" when the scroll switches direction

--- scripts/title_background_scroll.gd
+++ scripts/title_background_scroll.gd
@@ -1,15 +1,29 @@
-var screensize
+
+extends Sprite
+
+var region
+var bricklayer
+var texture_width
 var rate
+var vel
 
 func _ready():
-	set_process(true)
-	screensize = get_viewport_rect().size
-	rate = -25
+	set_fixed_process(true)
+	bricklayer = get_node("../BrickLayer")
+	texture_width = get_texture().get_width()
+	rate = 150
 
-func _process(delta):
-	var left_margin = self.get_margin(MARGIN_LEFT)
-	if(left_margin + self.get_rect().size.x + 1 < screensize.x && rate < 0):
-		rate = rate * -1
-	if(left_margin > -1 && rate > 0):
-		rate = rate * -1
-	self.set_margin(MARGIN_LEFT, left_margin + (rate * delta))
+func _fixed_process(delta):
+	region = get_region_rect()
+	if(region.pos.x + region.size.width + 1 >= texture_width && rate > 0):
+		rate = rate * -1 # pan lefy
+		nudge_bricks(rate * -1)
+	if(region.pos.x - 1 <= 0 && rate < 0):
+		rate = rate * -1 # pan right
+		nudge_bricks(rate * -1)
+	set_region_rect(Rect2(Vector2(region.pos.x + (rate * delta), region.pos.y), Vector2(region.size.width, region.size.height)))
+
+func nudge_bricks(impulse):
+	for b in bricklayer.get_children():
+		vel = b.get_linear_velocity()
+		b.set_linear_velocity(Vector2(vel.x + impulse, vel.y))
